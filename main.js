ae5a1ce1f86f21f0827e6c865ee96b00cf95def3
Violet Shreve <shreve@umich.edu>
2023-10-10 15:31:28 -0400

    Install @octokit/rest, correct usage of libraries

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const core = require("@actions/core");
 const { App } = require("@octokit/app");
+const { Octokit } = require("@octokit/rest");
 
 async function main() {
   const appId = core.getInput("app_id");
   const privateKey = core.getInput("private_key");
   const installationId = core.getInput("installation_id");
 
-  const app = new App({ appId, privateKey });
-  const octokit = await app.getInstallationOctokit(installationId);
+  const app = new App({ appId, privateKey, Octokit });
 
-  const token = octokit.rest.apps.createInstallationAccessToken();
+  const response = await app.octokit.rest.apps.createInstallationAccessToken({
+    installation_id: installationId,
+  });
+
+  const token = response.data.token;
 
   core.exportVariable("TOKEN", token);
   core.setSecret("TOKEN");
